7cfe7da435339afdd141361fe451004241092bdf
AdrielAcelainea <22016845@myrp.edu.sg>
2023-08-16 11:08:04 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22016845/demoweather/Weather.java
@@ -0,0 +1,31 @@
+package sg.edu.rp.c346.id22016845.demoweather;
+
+public class Weather {
+    private String area;
+    private String forecast;
+
+    public Weather(String area, String forecast){
+        this.area=area;
+        this.forecast=forecast;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getForecast() {
+        return forecast;
+    }
+
+    public void setForecast(String forecast) {
+        this.forecast = forecast;
+    }
+
+    public String toString(){
+        return "Area: "+ area +", Forecast: "+ forecast;
+    }
+}
